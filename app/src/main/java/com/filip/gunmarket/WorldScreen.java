5f171f92be49709b3272da2356158e1144b8daa8
Merdemg <merdemgunay88@gmail.com>
2015-11-13 22:56:59 -0500

    landscape & worldscreen frame

---
+++ app/src/main/java/com/filip/gunmarket/WorldScreen.java
@@ -0,0 +1,42 @@
+package com.filip.gunmarket;
+
+import com.filip.androidgames.framework.Game;
+import com.filip.androidgames.framework.Graphics;
+import com.filip.androidgames.framework.Screen;
+
+/**
+ * Created by Erdem on 2015-11-13.
+ */
+public class WorldScreen extends Screen {
+    public WorldScreen(Game game) {
+        super(game);
+    }
+
+
+
+    @Override
+    public void update(float deltaTime) {
+        Graphics g = game.getGraphics();
+
+    }
+
+    @Override
+    public void present(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
