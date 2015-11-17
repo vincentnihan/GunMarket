bb4f3b3d25be86bb69cf15892e86cd444c5eb7f0
Merdemg <merdemgunay88@gmail.com>
2015-11-16 22:03:04 -0500

    country and region screens
    FRAMEWORK

---
+++ app/src/main/java/com/filip/gunmarket/RegionScreen.java
@@ -0,0 +1,33 @@
+package com.filip.gunmarket;
+
+import com.filip.androidgames.framework.Game;
+import com.filip.androidgames.framework.Screen;
+
+/**
+ * Created by Erdem on 2015-11-16.
+ */
+public class RegionScreen extends Screen {
+    public RegionScreen(Game game) {
+        super(game);
+    }
+
+    public  void update(float deltaTime){
+
+    }
+    public  void present(float deltaTime){
+
+    }
+    public  void pause(){
+
+    }
+    public  void resume(){
+
+    }
+    public  void dispose(){
+
+    }
+
+
+
+
+}
