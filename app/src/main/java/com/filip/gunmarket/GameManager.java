4273f4bec14aed3f8b552cacf08f3cf75e9475f7
vincentnihan <nihanyang@hotmail.com>
2015-12-07 21:46:59 -0500

    time method update

--- app/src/main/java/com/filip/gunmarket/GameManager.java
+++ app/src/main/java/com/filip/gunmarket/GameManager.java
@@ -56,8 +56,49 @@ public class GameManager {
         if (doomsdayCounter<0)
             doomsdayCounter=0;
     }
-
+    public void TimeChange()
+    {
+        month++;
+        if(month >= 13)
+        {
+            month = 1;
+            year++;
+        }
+    }
+    public String getTime()
+    {
+        switch(month)
+        {
+            case 1:
+                return "January " + year;
+            case 2:
+                return "February " + year;
+            case 3:
+                return "March " + year;
+            case 4:
+                return "April " + year;
+            case 5:
+                return "May " + year;
+            case 6:
+                return "June " + year;
+            case 7:
+                return "July " + year;
+            case 8:
+                return "August " + year;
+            case 9:
+                return "September " + year;
+            case 10:
+                return "October " + year;
+            case 11:
+                return "November " + year;
+            case 12:
+                return "December " + year;
+            default:
+                return "Error";
+        }
+    }
     public void newTurn(){
+        TimeChange();
         if (doomsdayCounter >= 1){
             //END GAME
         }
