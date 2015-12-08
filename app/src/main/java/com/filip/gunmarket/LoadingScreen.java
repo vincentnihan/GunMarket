048f8e7118a3b5120b3e71e6e7f083dc22a442d0
vincentnihan <nihanyang@hotmail.com>
2015-12-08 09:01:07 -0500

    sound fix

--- app/src/main/java/com/filip/gunmarket/LoadingScreen.java
+++ app/src/main/java/com/filip/gunmarket/LoadingScreen.java
@@ -21,12 +21,10 @@ public class LoadingScreen extends Screen
     public void update(float deltaTime) {
         Graphics g = game.getGraphics();
 
-        //Assets.Background1  = game.getAudio().newSound("Background1.mp3");
+        Assets.Background1  = game.getAudio().newSound("Background1.mp3");
         //Assets.Background2 = game.getAudio().newSound("Background2.mp3");
         //Assets.Background3 = game.getAudio().newSound("Background3.mp3");
-        //Assets.menuSlide = game.getAudio().newSound("Menu.mp3");
-        Assets.Background1  = game.getAudio().newSound("Bubble Forming.mp3");
-        Assets.menuSlide = game.getAudio().newSound("Bubble Forming.mp3");
+        Assets.menuSlide = game.getAudio().newSound("Menu.mp3");
         Assets.bubblePop = game.getAudio().newSound("Bubble Pop.mp3");
         Assets.bubbleForm = game.getAudio().newSound("Bubble Forming.mp3");
 
